e069dd8ce453885589c53025752ad409a3190a79
yukiao <lembang19sony@gmail.com>
2020-09-25 13:02:12 +0800

    Adding query parameters

--- src/script/controller/loadStorage.js
+++ src/script/controller/loadStorage.js
@@ -1,5 +1,36 @@
+function getPosts(){
+    return fetch('/src/data/Posts.json')
+        .then(response => response.json())
+        .then(response => response);   
+}
+
+async function generateResult(keyword){
+    const posts = await getPosts();
+    let output = "";
+
+    posts.forEach(post => {
+        const name = post.title;
+        if(name.toLowerCase().includes(keyword)){
+            output += `
+            <div class="post">
+                <img src="${post.img}" alt="image">
+                <p class="image-title">${post.title}</p>
+                <a class="view" href="${post.link}">View</a>
+            </div>
+            `
+        }
+    });    
+    document.querySelector('.result').innerHTML = output;
+
+}
+
 document.addEventListener('DOMContentLoaded',function(){
-    let data = localStorage.getItem('output');
-    document.querySelector('.result').innerHTML = data;
-    localStorage.clear();
+    const query = {}
+    
+    let url = new URL(window.location.href)
+    url.searchParams.forEach((value,name)=>{
+        query[name] = value;
+    })
+    generateResult(query.keyword);
 })
+
--- src/script/controller/search.js
+++ src/script/controller/search.js
@@ -2,36 +2,41 @@ const header = document.querySelector('website-header').shadowRoot;
 header.addEventListener('click',function(e){
     if(e.target.parentElement.classList.contains('search-btn-side')){
         const keyword = header.querySelector('.search-text-side').value.toLowerCase();
-        generateResult(keyword);
+        openSearch(keyword);
     }else if(e.target.parentElement.classList.contains('search-btn')){
         const keyword = header.querySelector('.search-text').value.toLowerCase();
-        generateResult(keyword);
+        openSearch(keyword);
     }
 })
 
-function getPosts(){
-    return fetch('/src/data/Posts.json')
-        .then(response => response.json())
-        .then(response => response);   
-}
+// function getPosts(){
+//     return fetch('/src/data/Posts.json')
+//         .then(response => response.json())
+//         .then(response => response);   
+// }
 
-async function generateResult(keyword){
-    const posts = await getPosts();
-    let output = "";
+// async function generateResult(keyword){
+//     // const posts = await getPosts();
+//     // let output = "";
 
-    posts.forEach(post => {
-        const name = post.title;
-        if(name.toLowerCase().includes(keyword)){
-            output += `
-            <div class="post">
-                <img src="${post.img}" alt="image">
-                <p class="image-title">${post.title}</p>
-                <a class="view" href="${post.link}">View</a>
-            </div>
-            `
-        }
-    });
+//     // posts.forEach(post => {
+//     //     const name = post.title;
+//     //     if(name.toLowerCase().includes(keyword)){
+//     //         output += `
+//     //         <div class="post">
+//     //             <img src="${post.img}" alt="image">
+//     //             <p class="image-title">${post.title}</p>
+//     //             <a class="view" href="${post.link}">View</a>
+//     //         </div>
+//     //         `
+//     //     }
+//     // });
 
-    localStorage.setItem('output',output)
-    window.open('/search.html','_self');
+//     // localStorage.setItem('output',output)
+    
+// }
+function openSearch(keyword){
+    let url = new URL("https://yukiao.github.io/search.html")
+    url.searchParams.append('keyword',keyword);
+    window.open(url.href,'_self');
 }
